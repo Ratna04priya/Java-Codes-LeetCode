403836072ed105d62d6a32593825e03c5be42fba
Ratna Priya <ratnpriya1998@gmail.com>
2021-06-13 21:00:25 +0530

    Create 275. H-Index II.java

---
+++ LeetCode Medium/275. H-Index II.java
@@ -0,0 +1,20 @@
+// 275. H-Index II
+//By Ratna Priya
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low =0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(citations[mid] == (n-mid))
+                return citations[mid];
+            else if(citations[mid]>(n-mid))
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return n-low;
+    }
+}
