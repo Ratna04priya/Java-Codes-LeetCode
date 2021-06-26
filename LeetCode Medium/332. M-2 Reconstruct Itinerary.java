1549d3b1c2640a150bceca49db84c50807804ddb
Ratna Priya <ratnpriya1998@gmail.com>
2021-06-26 10:08:24 +0530

    Update 332. M-2 Reconstruct Itinerary.java

--- LeetCode Medium/332. M-2 Reconstruct Itinerary.java
+++ LeetCode Medium/332. M-2 Reconstruct Itinerary.java
@@ -18,3 +18,87 @@ class Solution {
         return ans;
     }
 }
+
+/*
+This is the variation of dfs.
+The way to tweak the dfs is to not to put a base condition and let the dfs fill and tsack and empty itslef
+What i mean by this is 
+
+Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Lets say we built a graph of this and its adjacenecy list look like this:-
+JFK - MUC
+MUC - LHR
+LHR - SFO
+SFO - SJC
+
+So when we perform a dfs on JFK
+
+|        |
+|        |
+|        |   <-----Recursion Stack
+|        |
+|__JFK___|
+
+We explore JFKs neighbors 
+
+|        |
+|        |
+|        |
+|  MUC   |
+|__JFK___|
+
+We explore Muc neighbors
+
+|        |
+|        |
+|  LHR   |
+|  MUC   |
+|__JFK___|
+
+|        |
+|  SFO   |
+|  LHR   |
+|  MUC   |
+|__JFK___|
+
+|  SJC   |
+|  SFO   |
+|  LHR   |
+|  MUC   |
+|__JFK___|
+
+Since SJC has no neighbors we pus it back to the answer
+
+|        |
+|  SFO   |      ans=SJC
+|  LHR   |
+|  MUC   |
+|__JFK___|
+
+|        |
+|        |      ans=SJC,SFO
+|  LHR   |
+|  MUC   |
+|__JFK___|
+
+|        |
+|        |      ans=SJC,SFO,LHR
+|        |
+|  MUC   |
+|__JFK___|
+
+
+|        |
+|        |      ans=SJC,SFO,LHR,MUC 
+|        |
+|        |
+|__JFK___|
+
+|        |
+|        |      ans=SJC,SFO,LHR,MUC,JFK 
+|        |
+|        |
+|________|
+
+Print in reverse order
+*/
