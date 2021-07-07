e17fd37b50a82891078432a6c9e5edae06beaa53
Ratna Priya <ratnpriya1998@gmail.com>
2021-07-07 20:57:40 +0530

    Create 378.Kth Smallest Element in a Sorted Matrix.java

---
+++ July LeetCode Challenge 2021/Week 1/378.Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,16 @@
+//378.Kth Smallest Element in a Sorted Matrix
+// BY Ratna Priya
+
+class Solution {
+    public int kthSmallest(int[][] M, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for(int[] R : M){
+            for(int a : R){
+                heap.add(a);
+                if(heap.size() > k)
+                    heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
