807027570643db2ed0fd2c29ce7d4ad69ee6ab74
Ratna Priya <ratnpriya1998@gmail.com>
2021-07-17 10:12:46 +0530

    Create 611. Valid Triangle Number.java

---
+++ LeetCode Medium/611. Valid Triangle Number.java
@@ -0,0 +1,27 @@
+//611. Valid Triangle Number
+//By Ratna Priya
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int k=nums.length-1;k>=2;k--)
+        {
+            int i=0;
+            int j=k-1;
+            while(i<j)
+            {
+                if(nums[i]+nums[j]>nums[k])
+                {
+                    count+=j-i;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }         
+            }
+        }
+        return count;
+    }
+}
