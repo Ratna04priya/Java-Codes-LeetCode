7b974857e6d87adc260d0ffb09e4cb9774487c08
Ratna Priya <ratnpriya1998@gmail.com>
2021-07-30 21:00:54 +0530

    Create 677. Map Sum Pairs.java

---
+++ July LeetCode Challenge 2021/Week 5/677. Map Sum Pairs.java
@@ -0,0 +1,21 @@
+// 677. Map Sum Pairs
+// By Ratna Priya
+
+class MapSum {
+    HashMap<String, Integer> map;
+    public MapSum() {
+        map = new HashMap<>();
+    }
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+    public int sum(String prefix) {
+        int ans = 0;
+        for (String key: map.keySet()) {
+            if (key.startsWith(prefix)) {
+                ans += map.get(key);
+            }
+        }
+        return ans;
+    }
+}
