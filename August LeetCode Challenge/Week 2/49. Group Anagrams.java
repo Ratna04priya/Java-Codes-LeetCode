a0c0871cafae8e12af525706e2a489c9a9509204
Ratna Priya <ratnpriya1998@gmail.com>
2021-08-12 14:00:22 +0530

    Create 49. Group Anagrams.java

---
+++ August LeetCode Challenge/Week 2/49. Group Anagrams.java
@@ -0,0 +1,16 @@
+//49. Group Anagrams
+//By Ratna Priya
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {        
+        Map<String,List<String>> map = new HashMap<String,List<String>>();
+        for (String string:strs){
+            char[] chars = string.toCharArray();
+            Arrays.sort(chars);
+            String sortedString = new String(chars);            
+            map.putIfAbsent(sortedString, new ArrayList<String>());            
+            map.get(sortedString).add(string);
+        }
+        return new ArrayList(map.values());
+    }
+}
