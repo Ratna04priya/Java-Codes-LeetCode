3ca0c2e26e656650ab069bbe53784263b3bd9e74
Ratna Priya <ratnpriya1998@gmail.com>
2021-08-25 13:44:33 +0530

    Create 633. Sum of Square Numbers.java

---
+++ August LeetCode Challenge/Week 4/633. Sum of Square Numbers.java
@@ -0,0 +1,19 @@
+//633. Sum of Square Numbers
+//By Ratna Priya
+
+public class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
+    }
+}
