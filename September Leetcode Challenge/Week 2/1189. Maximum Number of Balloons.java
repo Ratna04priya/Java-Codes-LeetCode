81bc8460a7cc37e78abaca633af32e84ab1f9908
Ratna Priya <ratnpriya1998@gmail.com>
2021-09-13 17:21:06 +0530

    Create 1189. Maximum Number of Balloons.java

---
+++ September Leetcode Challenge/Week 2/1189. Maximum Number of Balloons.java
@@ -0,0 +1,31 @@
+//1189. Maximum Number of Balloons
+//By Ratna Priya
+
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int bCount = 0, aCount = 0, lCount = 0, oCount = 0, nCount = 0;
+
+        // Count the frequencey of all the five characters
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 'b') {
+                bCount++;
+            } else if (text.charAt(i) == 'a') {
+                aCount++;
+            } else if (text.charAt(i) == 'l') {
+                lCount++;
+            } else if (text.charAt(i) == 'o') {
+                oCount++;
+            } else if (text.charAt(i) == 'n') {
+                nCount++;
+            }
+        }
+	    
+        // Find the potential of each character.
+        // Except for 'l' and 'o' the potential is equal to the frequency.
+        lCount = lCount / 2;
+        oCount = oCount / 2;
+        
+        // Find the bottleneck.
+        return Math.min(bCount, Math.min(aCount, Math.min(lCount, Math.min(oCount, nCount))));
+    }
+}
