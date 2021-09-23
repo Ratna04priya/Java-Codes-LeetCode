c3639a5f6c7a59d8bc48a12d11461c4018ed13c4
Ratna Priya <ratnpriya1998@gmail.com>
2021-09-23 23:18:04 +0530

    Create 1328. Break a Palindrome.java

---
+++ LeetCode Medium/1328. Break a Palindrome.java
@@ -0,0 +1,23 @@
+//1328. Break a Palindrome
+//BBy Ratna Priya
+
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int length = palindrome.length();
+        if (length == 1) { 
+            return "";
+        }
+        // Strings are immutable in Java, convert it into a char array
+        char[] palindromeArray = palindrome.toCharArray();
+        
+        for (int i = 0; i < length / 2; i++) {
+            if (palindromeArray[i] != 'a') {
+                palindromeArray[i] = 'a';
+                return String.valueOf(palindromeArray);
+            }
+        }
+        
+        palindromeArray[length - 1] = 'b';
+        return String.valueOf(palindromeArray);
+    }
+}
