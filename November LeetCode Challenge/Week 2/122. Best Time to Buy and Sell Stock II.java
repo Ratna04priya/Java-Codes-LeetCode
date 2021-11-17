404e93c6c7439b8607e53dfbea42e94e58088bed
Ratna Priya <ratnpriya1998@gmail.com>
2021-11-17 12:32:54 +0530

    Create 122. Best Time to Buy and Sell Stock II.java

---
+++ November LeetCode Challenge/Week 2/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,17 @@
+//122. Best Time to Buy and Sell Stock II
+//By Ratna Priya
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices ==null || prices.length ==0){
+            return 0;
+        }
+        int profit =0;
+        for(int i = 0; i<prices.length-1;i++){
+            if(prices[i+1]> prices[i]){
+                profit +=prices[i+1]-prices[i];
+            }
+        }
+        return profit;
+    }
+}
