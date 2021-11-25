d2f0a600a6b7cfe39afa8cd8aa0c6aa14edbcf02
Ratna Priya <ratnpriya1998@gmail.com>
2021-11-25 16:32:43 +0530

    Create 53. Maximum Subarray.java

---
+++ LeetCode Easy/53. Maximum Subarray.java
@@ -0,0 +1,17 @@
+//53. Maximum Subarray
+// By Ratna Priya
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+   int storage []=  new int[nums.length];
+   int max  =  nums[0];
+   storage[0 ] =  nums[0];
+   for( int i=1;i<nums.length ;i++  ){
+       storage[i] =  Math.max( storage[i-1]+nums[i] ,  nums[i]);
+       if(storage[i]>max){
+           max=  storage[i];
+       }
+   }
+   return max;    
+     }
+}
